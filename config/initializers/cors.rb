7114a0e7169a0087d52fa1dc293a25010a073299
aresnik11 <aresnik11@gmail.com>
2019-11-20 20:51:33 -0500

    finally fixed limited cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://best-set-game.netlify.com'
 
     resource '*',
       headers: :any,
